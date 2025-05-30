63112a04582db59fb0556345e2cf1e89266b7af1
lurkydismal <lurkydismal@duck.com>
2025-05-30 01:46:06 +0000

    1

--- camera_t/include/camera_t.h
+++ camera_t/include/camera_t.h
@@ -7,11 +7,18 @@
 #define DEFAULT_CAMERA { .worldX = 0, .worldY = 0 }
 
 typedef struct camera_t {
+    float zoom;
+    float zoomMin;
+    float zoomMax;
     float worldX;
     float worldY;
+    float width;
+    float height;
+    float logicalWidth;
+    float logicalHeight;
 } camera_t;
 
 camera_t camera_t$create( void );
 bool camera_t$destroy( camera_t* _camera );
 
-bool camera_t$update( player_t* _player );
+bool camera_t$update( camera_t* _camera, player_t* _player );
