3328d1c76aea3da1ed84b5d2c0cde11825808e99
lurkydismal <lurkydismal@duck.com>
2025-10-07 08:51:57 +0500

    Move ctre to submodule

--- ctre/include/ctre.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "ctre/functions.hpp"
-#include "ctre/iterators.hpp"
-#include "ctre/literals.hpp"
-#include "ctre/operators.hpp"
-#include "ctre/range.hpp"
--- ctre/include/unicode-db.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define CTRE_UNICODE_SYNOPSYS_WAS_INCLUDED
-#include "unicode-db/unicode-db.hpp"
